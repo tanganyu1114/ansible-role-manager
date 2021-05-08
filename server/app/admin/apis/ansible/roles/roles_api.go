d628536432f47c1af11bcd4613ea4d56dcf3611d
ClessLi <316600949@qq.com>
2021-05-08 15:32:42 +0800

    初步完成roles api接口

--- server/app/admin/apis/ansible/roles/roles_api.go
+++ server/app/admin/apis/ansible/roles/roles_api.go
@@ -2,6 +2,7 @@ package roles
 
 import (
 	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/tanganyu1114/ansible-role-manager/common/apis"
 	"io/ioutil"
@@ -46,29 +47,62 @@ func (r *rolesApi) AddRoleByCompressedData(c *gin.Context) {
 	}
 	roleDataFile, err := roleDataFileHeader.Open()
 	if err != nil {
-		r.Error(c, http.StatusBadRequest, err, "role文件无法打开")
+		r.Error(c, http.StatusUnsupportedMediaType, err, "role文件无法打开")
 		return
 	}
 	defer roleDataFile.Close()
 	roleData, err := ioutil.ReadAll(roleDataFile)
 	if err != nil {
-		r.Error(c, http.StatusBadRequest, err, "读取role文件失败")
+		r.Error(c, http.StatusUnsupportedMediaType, err, "读取role文件失败")
 		return
 	}
 	err = r.vo.ImportRoleData(roleName, roleData)
 	if err != nil {
-		r.Error(c, http.StatusBadRequest, err, "导入role文件失败")
+		r.Error(c, http.StatusUnsupportedMediaType, err, "导入role文件失败")
 		return
 	}
 	r.OK(c, nil, "导入role文件成功")
 }
 
 func (r *rolesApi) DownloadRoleCompressedData(c *gin.Context) {
-	// TODO: 下载role
-	panic("implement me")
+	// DONE: 下载role
+	// method: GET location: /:role
+
+	roleName := c.Param("role")
+	if strings.TrimSpace(roleName) == "" {
+		r.Error(c, http.StatusNotFound, errors.New("role name is null"), "指定的role为空")
+		return
+	}
+
+	roleData, err := r.vo.ExportRoleData(roleName)
+	if err != nil {
+		r.Error(c, http.StatusForbidden, err, "生成role压缩文件失败")
+		return
+	}
+	c.Header("Content-Type", "application/zip")
+	c.Header("Content-Disposition", fmt.Sprintf("attachment; filename=%s.zip", roleName))
+	c.Header("Content-Transfer-Encoding", "binary")
+	_, err = c.Writer.Write(roleData)
+	if err != nil {
+		r.Error(c, http.StatusNotImplemented, err, "导出role压缩文件失败")
+	}
+	//r.OK(c, nil, "导出role文件成功")
 }
 
 func (r *rolesApi) RemoveRole(c *gin.Context) {
-	// TODO: 删除role
-	panic("implement me")
+	// DONE: 删除role
+	// method: GET location: /:role
+
+	roleName := c.Param("role")
+	if strings.TrimSpace(roleName) == "" {
+		r.Error(c, http.StatusNotFound, errors.New("role name is null"), "指定的role为空")
+		return
+	}
+
+	err := r.vo.RemoveRole(roleName)
+	if err != nil {
+		r.Error(c, http.StatusNotFound, err, "删除role文件失败")
+		return
+	}
+	r.OK(c, nil, "删除role文件成功")
 }
